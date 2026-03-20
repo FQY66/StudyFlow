90f21a146d9e9a42e5a859d2ce96374742260f5a
FQY <935947429@qq.com>
2026-03-20 16:16:25 +0800

    增加默认头像

---
+++ sf-fronted/src/utils/request.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import { API_BASE_URL } from '@/config/api'
+
+const request = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 10000
+})
+
+request.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers = config.headers || {}
+      config.headers.token = token
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+export default request
